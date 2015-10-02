5a530e8bceb0801d0bb98d3b99926cd3d6c2e99e
danielrbetteridge@gmail.com <danielrbetteridge@gmail.com>
2015-10-03 00:10:46 +0800

    attempt to increase accuracy of geoAPI

--- static/js/getLocation.js
+++ static/js/getLocation.js
@@ -1,7 +1,15 @@
 function getLocation() {
 	var x = document.getElementById("demo");
     if (navigator.geolocation) {
-        navigator.geolocation.watchPosition(showPosition);
+        navigator.geolocation.watchPosition(
+        	showPosition,
+        	positionError,
+        	{
+        		timeout: 0,
+        		enableHighAccuracy: true,
+        		maximumAge: Infinity
+        	}
+        	);
     } else {
         x.innerHTML = "Geolocation is not supported by this browser.";
     }
