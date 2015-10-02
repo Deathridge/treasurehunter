1be4280e3e056bf9e11db260f39ebad27cf71472
danielrbetteridge@gmail.com <danielrbetteridge@gmail.com>
2015-10-02 23:02:55 +0800

    setting up for geolocation project

---
+++ static/js/getLocation.js
@@ -0,0 +1,15 @@
+function getLocation() {
+	var x = document.getElementById("demo");
+    if (navigator.geolocation) {
+        navigator.geolocation.watchPosition(showPosition);
+    } else {
+        x.innerHTML = "Geolocation is not supported by this browser.";
+    }
+}
+function showPosition(position) {
+	var x = document.getElementById("demo");
+    x.innerHTML = "Latitude: " + position.coords.latitude + 
+    "<br>Longitude: " + position.coords.longitude; 
+}
+
+setInterval(getLocation(), 500);
