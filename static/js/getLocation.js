ea235c4bbd62964ecf77b40967a6c49c06c15610
danielrbetteridge@gmail.com <danielrbetteridge@gmail.com>
2015-10-03 00:21:59 +0800

    attempt to increase accuracy of geoAPI

--- static/js/getLocation.js
+++ static/js/getLocation.js
@@ -51,4 +51,22 @@ function errorCallback_highAccuracy(position) {
    document.getElementById("demo").append(msg);
 }
 
+function errorCallback_lowAccuracy(position) {
+    var msg = "<p>Can't get your location (low accuracy attempt). Error = ";
+    if (error.code == 1)
+        msg += "PERMISSION_DENIED";
+    else if (error.code == 2)
+        msg += "POSITION_UNAVAILABLE";
+    else if (error.code == 3)
+        msg += "TIMEOUT";
+    msg += ", msg = "+error.message;
+    
+    $('body').append(msg);
+}
+
+function successCallback(position) {
+    var latitude = position.coords.latitude;
+    var longitude = position.coords.longitude;
+    $('body').append("<p>Your location is: " + latitude + "," + longitude+" </p><p>Accuracy="+position.coords.accuracy+"m");
+}
 setInterval(getLocation(), 500);
