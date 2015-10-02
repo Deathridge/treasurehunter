196001cfb943bfe5d20074d4d50421e826bd8664
danielrbetteridge@gmail.com <danielrbetteridge@gmail.com>
2015-10-03 00:20:13 +0800

    attempt to increase accuracy of geoAPI

--- static/js/getLocation.js
+++ static/js/getLocation.js
@@ -2,7 +2,8 @@ function getLocation() {
 	var x = document.getElementById("demo");
     if (navigator.geolocation) {
         navigator.geolocation.watchPosition(
-        	showPosition,        	
+        	showPosition,  
+        	errorCallback_highAccuracy,      	
         	{
         		timeout: 0,
         		enableHighAccuracy: true,
@@ -25,6 +26,29 @@ function showPosition(position) {
     "<br>heading: " + position.coords.heading
     + 
     "<br>speed: " + position.coords.speed; 
+}
+
+function errorCallback_highAccuracy(position) {
+    if (error.code == error.TIMEOUT)
+    {
+        // Attempt to get GPS loc timed out after 5 seconds, 
+        // try low accuracy location
+        document.getElementById("demo").append("attempting to get low accuracy location");
+        navigator.geolocation.getCurrentPosition(
+               successCallback, 
+               errorCallback_lowAccuracy,
+               {maximumAge:600000, timeout:10000, enableHighAccuracy: false});
+        return;
+    }
+    
+    var msg = "<p>Can't get your location (high accuracy attempt). Error = ";
+    if (error.code == 1)
+        msg += "PERMISSION_DENIED";
+    else if (error.code == 2)
+        msg += "POSITION_UNAVAILABLE";
+    msg += ", msg = "+error.message;
+    
+   document.getElementById("demo").append(msg);
 }
 
 setInterval(getLocation(), 500);
