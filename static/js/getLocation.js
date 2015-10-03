3235383004e36cfeeb0b4452f9752073652a70ef
danielrbetteridge@gmail.com <danielrbetteridge@gmail.com>
2015-10-03 12:43:58 +0800

    test p tag for location info

--- static/js/getLocation.js
+++ static/js/getLocation.js
@@ -77,6 +77,17 @@ function successCallback(position) {
 function updateMap(position){
 	L.mapbox.accessToken = 'pk.eyJ1IjoiZGFuaWVsYmV0dGVyaWRnZSIsImEiOiJjaWY3bjZqazcwc3IzczdrcmU1NjJ1czdnIn0.Xr0sZHMxs6Fvp7lzmmtJSg';
 	var map = L.mapbox.map('map', 'mapbox.streets').setView([position.coords.latitude, position.coords.longitude], 18);
+	var x = document.getElementById("demo");
+	x.innerHTML = "Latitude: " + position.coords.latitude + 
+    "<br>Longitude: " + position.coords.longitude
+    + 
+    "<br>accuracy: " + position.coords.accuracy
+    + 
+    "<br>altitude: " + position.coords.altitude
+    + 
+    "<br>heading: " + position.coords.heading
+    + 
+    "<br>speed: " + position.coords.speed;
 };
 
 
