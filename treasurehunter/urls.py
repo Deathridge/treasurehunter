6ae3053c920c0d6bc07e356ba36a517926f9bd02
danielrbetteridge@gmail.com <danielrbetteridge@gmail.com>
2015-10-03 11:10:12 +0800

    added mapbox css and js files, added code to locate map onto user

--- treasurehunter/urls.py
+++ treasurehunter/urls.py
@@ -8,5 +8,5 @@ urlpatterns = patterns('',
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^$', TemplateView.as_view(template_name="base.html") ),
+    url(r'^$', TemplateView.as_view(template_name="map.html") ),
 )
