3e391ff3cba3913bf577ace92eab27abd4ec8aad
danielrbetteridge@gmail.com <danielrbetteridge@gmail.com>
2015-10-03 11:57:29 +0800

    testing with different returns

--- static/js/getLocation.js
+++ static/js/getLocation.js
@@ -75,7 +75,6 @@ function successCallback(position) {
 }
 
 function updateMap(position){
-	console.log(latitude);
 	L.mapbox.accessToken = 'danielbetteridge.cif7n6i1q0sptsolzat1vewt9';
 	var map = L.mapbox.map('map', 'mapbox.streets').setView([position.coords.latitude, position.coords.longitude], 9);
 };
