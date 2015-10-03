11112ff7be6ac11a5465e87886df9ee6e580f144
danielrbetteridge@gmail.com <danielrbetteridge@gmail.com>
2015-10-03 13:51:07 +0800

    auto update location/map

--- static/js/getLocation.js
+++ static/js/getLocation.js
@@ -3,7 +3,7 @@ function getLocation() {
 	
     if (navigator.geolocation) {
         navigator.geolocation.watchPosition(
-        	updateMap,  
+        	successCallback,  
         	errorCallback_highAccuracy,      	
         	{
         		timeout: 10000,
@@ -74,38 +74,7 @@ function successCallback(position) {
     
 }
 
-function updateMap(position){
-	L.mapbox.accessToken = 'pk.eyJ1IjoiZGFuaWVsYmV0dGVyaWRnZSIsImEiOiJjaWY3bjZqazcwc3IzczdrcmU1NjJ1czdnIn0.Xr0sZHMxs6Fvp7lzmmtJSg';
-	var map = L.mapbox.map('map', 'mapbox.streets').setView([position.coords.latitude, position.coords.longitude], 18);
 
-
-	L.mapbox.featureLayer({
-    // this feature is in the GeoJSON format: see geojson.org
-    // for the full specification
-    type: 'Feature',
-    geometry: {
-        type: 'Point',
-        // coordinates here are in longitude, latitude order because
-        // x, y is the standard for GeoJSON and many formats
-        coordinates: [
-          position.coords.longitude,
-          position.coords.latitude 
-        ]
-    }
-    
-	}).addTo(map);
-	var x = document.getElementById("demo");
-	x.innerHTML = "Latitude: " + position.coords.latitude + 
-    "<br>Longitude: " + position.coords.longitude
-    + 
-    "<br>accuracy: " + position.coords.accuracy
-    + 
-    "<br>altitude: " + position.coords.altitude
-    + 
-    "<br>heading: " + position.coords.heading
-    + 
-    "<br>speed: " + position.coords.speed;
-};
 
 
 getLocation();
--- static/js/map.js
+++ static/js/map.js
@@ -1,7 +1,54 @@
+function updateMap() {
+	var x = document.getElementById("demo");
+	
+    if (navigator.geolocation) {
+        navigator.geolocation.watchPosition(
+        	successUpdate,  
+        	errorCallback_highAccuracy,      	
+        	{
+        		timeout: 10000,
+        		enableHighAccuracy: true,
+        		maximumAge: Infinity
+        	}
+        	);
+    } else {
+        x.innerHTML = "Geolocation is not supported by this browser.";
+    }
 
-function updateMap(){
+   
+}
 
-L.mapbox.accessToken = 'pk.eyJ1IjoiZGFuaWVsYmV0dGVyaWRnZSIsImEiOiJjaWY3bjZqazcwc3IzczdrcmU1NjJ1czdnIn0.Xr0sZHMxs6Fvp7lzmmtJSg';
-var map = L.mapbox.map('map', 'mapbox.streets').setView([latitude, longitude], 9);
+function successUpdate(position){
+	L.mapbox.accessToken = 'pk.eyJ1IjoiZGFuaWVsYmV0dGVyaWRnZSIsImEiOiJjaWY3bjZqazcwc3IzczdrcmU1NjJ1czdnIn0.Xr0sZHMxs6Fvp7lzmmtJSg';
+	var map = L.mapbox.map('map', 'mapbox.streets').setView([position.coords.latitude, position.coords.longitude], 22);
+
+
+	L.mapbox.featureLayer({
+    // this feature is in the GeoJSON format: see geojson.org
+    // for the full specification
+    type: 'Feature',
+    geometry: {
+        type: 'Point',
+        // coordinates here are in longitude, latitude order because
+        // x, y is the standard for GeoJSON and many formats
+        coordinates: [
+          position.coords.longitude,
+          position.coords.latitude 
+        ]
+    }
+    
+	}).addTo(map);
+	var x = document.getElementById("demo");
+	x.innerHTML = "Latitude: " + position.coords.latitude + 
+    "<br>Longitude: " + position.coords.longitude
+    + 
+    "<br>accuracy: " + position.coords.accuracy
+    + 
+    "<br>altitude: " + position.coords.altitude
+    + 
+    "<br>heading: " + position.coords.heading
+    + 
+    "<br>speed: " + position.coords.speed;
 };
 
+setInterval(updateMap(), 1000);
