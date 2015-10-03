2b8da7781be6c520041e2cd4ab5dbdb0b49666eb
danielrbetteridge@gmail.com <danielrbetteridge@gmail.com>
2015-10-03 11:38:03 +0800

    testing with different returns

--- static/js/map.js
+++ static/js/map.js
@@ -3,6 +3,7 @@ var latitude = coor.latitude;
 var longitude = coor.longitude;
 
 function updateMap(){
+console.log(latitude);
 L.mapbox.accessToken = 'danielbetteridge.cif7n6i1q0sptsolzat1vewt9';
 var map = L.mapbox.map('map', 'mapbox.streets').setView([latitude, longitude], 9);
 };
